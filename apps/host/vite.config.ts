b9f5e5811de07b28e893f02ff159b428c6dcc7ea
Sarang <sarang1221@gmail.com>
2026-05-29 16:20:19 +0530

    changes for dev/production env variables

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -1,52 +1,61 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { federation } from "@module-federation/vite";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "host",
-      dts: false,
-      
-      remotes: {
-        catalog: {
-          type: "module",
-          name: "catalog",
-          entry: "http://localhost:3001/remoteEntry.js",
-          shareScope: "default",
-        },
-        checkout: {
-          type: "module",
-          name: "checkout",
-          entry: "http://localhost:3002/remoteEntry.js",
-          shareScope: "default",
-        },
-        cart: {
-          type: "module",
-          name: "cart",
-          entry: "http://localhost:3003/remoteEntry.js",
-          shareScope: "default",
-        }
-      },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-      shared: {
-        react: {
-          singleton: true,
-        },
-        "react-dom": {
-          singleton: true,
-        },
-      },
-    }),
-  ],
+  return {
+    plugins: [
+      react(),
 
-  server: {
-    port: 3000,
-    origin: "http://localhost:3000",
-  },
+      federation({
+        name: "host",
 
-  preview: {
-    port: 3000,
-  },
+        dts: false,
+
+        remotes: {
+          catalog: {
+            type: "module",
+            name: "catalog",
+            entry: env.VITE_CATALOG_REMOTE,
+            shareScope: "default",
+          },
+
+          checkout: {
+            type: "module",
+            name: "checkout",
+            entry: env.VITE_CHECKOUT_REMOTE,
+            shareScope: "default",
+          },
+
+          cart: {
+            type: "module",
+            name: "cart",
+            entry: env.VITE_CART_REMOTE,
+            shareScope: "default",
+          },
+        },
+
+        shared: {
+          react: {
+            singleton: true,
+          },
+
+          "react-dom": {
+            singleton: true,
+          },
+        },
+      }),
+    ],
+
+    server: {
+      port: 3000,
+      origin: "http://localhost:3000",
+    },
+
+    preview: {
+      port: 3000,
+    },
+  };
 });
