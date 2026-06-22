d05dc225bf538b7d73c31cea1bc869c2e848fc3e
Sarang <sarang1221@gmail.com>
2026-06-22 17:12:40 +0530

    Changes to landing page

--- apps/catalog/src/App.tsx
+++ apps/catalog/src/App.tsx
@@ -2,12 +2,19 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-screen bg-blue-100 p-10">
-      <h1 className="text-5xl font-bold text-blue-900">
+    <div className="flex w-full h-screen overflow-hidden bg-blue-100">
+      {/* <h1 className="text-5xl font-bold text-blue-900">
         Catalog Microfrontend
       </h1>
 
-      <p className="mt-4 text-xl">Running independently on port 3001</p>
+      <p className="mt-4 text-xl">Running independently on port 3001</p> */}
+
+      <div className="w-1/6 border-r-2 border-gray-300">
+        <h1>Filters</h1>
+      </div>
+      <div className="w-5/6">
+        <h1>Products</h1>
+      </div>
     </div>
   );
 }
--- apps/host/src/App.tsx
+++ apps/host/src/App.tsx
@@ -1,38 +1,30 @@
-import './App.css'
+import "./App.css";
 
 import React, { Suspense } from "react";
 
-const CatalogApp = React.lazy(
-  () => import("catalog/CatalogApp")
-);
+const CatalogApp = React.lazy(() => import("catalog/CatalogApp"));
 
-const CheckoutApp = React.lazy(
-  () => import("checkout/CheckoutApp")
-)
+// const CheckoutApp = React.lazy(() => import("checkout/CheckoutApp"));
 
-const CartApp = React.lazy(
-  () => import("cart/CartApp")
-)
+// const CartApp = React.lazy(() => import("cart/CartApp"));
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="flex flex-col h-screen bg-gray-100">
       <div className="bg-black px-6 py-4 text-white">
-        <h1 className="text-3xl font-bold">
-          Host Application
-        </h1>
+        <h1 className="text-3xl font-bold">Host Application</h1>
       </div>
 
-      <div className='flex column'>
+      <div className="flex flex-col flex-1 overflow-auto">
         <Suspense fallback={<div>Loading Catalog...</div>}>
-        <CatalogApp />
-      </Suspense>
-      <Suspense fallback={<div>Loading Checkout...</div>}>
-        <CheckoutApp />
-      </Suspense>
-      <Suspense fallback={<div>Loading Checkout...</div>}>
-        <CartApp/>
-      </Suspense>
+          <CatalogApp />
+        </Suspense>
+        {/* <Suspense fallback={<div>Loading Checkout...</div>}>
+          <CheckoutApp />
+        </Suspense> */}
+        {/* <Suspense fallback={<div>Loading Checkout...</div>}>
+          <CartApp />
+        </Suspense> */}
       </div>
     </div>
   );
